92fa94fbc8198faabe0aa45645ce1519bf1a27ca
Tomás <tmc.silva@campus.fct.unl.pt>
2026-09-20 19:34:38 +0100

    EP command (falta main)

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -94,7 +94,7 @@ public class ContactBook {
     }
 
     public boolean hasRepeatedNumbers(){
-        for (int i = 0; i<counter; i++ ){
+        for (int i = 0; i < counter; i++ ){
             for (int j = i+1; j<counter; j++ ){
                 if (contacts[i].getPhone() == contacts[j].getPhone())
                     return true;
